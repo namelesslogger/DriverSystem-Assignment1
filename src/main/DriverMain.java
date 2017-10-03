33ddaa22ef35e2891c2e78252ffdadfbe095e925
namelesslogger <johnshanie@gmail.com>
2017-10-03 22:11:40 +0100

    commented test code in main classes

--- src/main/DriverMain.java
+++ src/main/DriverMain.java
@@ -2,6 +2,46 @@ package main;
 
 public class DriverMain {
 	public static void main(String[] args){
-		//Student s = new Student();
+		//Student s1 = new Student();
+		//Student s2 = new Student();
+		//Student s3 = new Student();
+		//Student s4 = new Student();
+		//Student s5 = new Student();
+		//Student s6 = new Student();
+		//Student s7 = new Student();
+		//Student s8 = new Student();
+		
+		/*
+		  Module MA101 = new Module("Math", "MA101");
+		  Module MA102 = new Module("Algebra", "MA102");
+		  Module CT101 = new Module("Programming", "CT101");
+		  Module CT102 = new Module("Algorithms", "CT102");
+		  
+		  CourseProgramme mathProgramme = new CourseProgramme("Mathematics");
+		  CourseProgramme csProgramme = new CourseProgramme("Computer Science");
+		  
+		  mathProgramme.addModule(MA101);
+		  mathProgramme.addModule(MA102);
+		  csProgramme.addModule(CT101);
+		  csProgramme.addModule(CT102);
+		  
+		  MA101.addStudent(s1);
+		  MA101.addStudent(s2);
+		  MA101.addStudent(s3);
+		  MA101.addStudent(s4);
+		  CT101.addStudent(s5);
+		  CT101.addStudent(s6);
+		  CT101.addStudent(s7);
+		  CT101.addStudent(s8);
+		 
+		  System.out.println("Programme: " + mathProgramme.getName() + " module : " +);
+		  for(Module module : mathProgramme.getModuleList()){
+		  	System.out.print(module.getName() + " students : " +);
+		  	for(Student student: module.getStudentList()){
+		  		System.out.println(student.getName());
+		  	}
+		  }
+		  
+		 */
 	}
 }
